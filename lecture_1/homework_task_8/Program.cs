0ba87544fae0e1536257c6ed25ac68feeda35cc0
VadimTalimonchik <tvadmins@gmail.com>
2022-08-24 09:54:15 +0300

    Homework Task 8

---
+++ lecture_1/homework_task_8/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Задача 8
+
+Напишите программу, которая на вход
+принимает число (N), а на выходе показывает
+все чётные числа от 1 до N
+*/
+
+Console.WriteLine("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while (i <= numberA)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i + " - четное число в составе числа " + numberA);
+    }
+    i++;
+}
