5f7b3c9a484c90a430fd54a28cf507f7c5611fba
VadimTalimonchik <tvadmins@gmail.com>
2022-08-24 09:28:54 +0300

    Lecture_1

---
+++ lecture_1/homework_task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 2
+
+Напишите программу, которая на вход
+принимает два числа и выдаёт, какое
+число большее, а какое меньшее.
+*/
+
+Console.WriteLine("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB)
+{
+    Console.WriteLine("Число А больше чила В");
+}
+else
+{
+    Console.WriteLine("Число В больше чила А");
+}
